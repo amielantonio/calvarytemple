ba13491f8d0f7169c543b91681709c040a35adb8
amielantonio <amielnicole.antonio@gmail.com>
2018-01-15 09:18:53 +0800

    add auth base functions

---
+++ app/core/Auth.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* Authentication
+* 
+* Responsible for checking all authentication
+* and user process
+*/
+
+function login( $username, $password){
+
+}
+
+function auth( $user ){
+	
+}
+
+function current_user(){
+
+}
+
+function access_level( $user=""  ){
+	
+}
+
+function is_admin(){
+
+}
+
+function is_author(){
+
+}
+
+function user_settings(){
+	
+}
+
+
+/** helper functions*/
+
+function add_user_session( $user ){
+
+}
+
